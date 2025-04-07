23a7d85005b686fa559ebb19c3c79cb9c72df4e4
adamyrzakanova <aidaimyrzakanova@usf.edu>
2025-04-06 21:14:06 -0400

    git init

---
+++ src/components/Greetings.jsx
@@ -0,0 +1,14 @@
+import React from "react"; // Importing React to Create React Component
+
+// Creating a new component called Greetings
+
+function Greetings() {
+    return (
+        <div>
+            <h2>Hello from the Greeting Component!</h2>
+            <p>This is your first custom component in React.</p>
+        </div>
+    );
+}
+
+export default Greetings; // Exporting the Greetings component so it can be used in app.jsx
